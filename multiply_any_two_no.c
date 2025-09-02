8f0e2c837d3c144464263688bd5f561faba251d3
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-09-02 09:42:00 -0700

    Add files via upload

---
+++ multiply_any_two_no.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,multiply;
+    printf("enter first number:");
+    scanf(" %d",&a);
+     printf("enter second number:");
+    scanf (" %d",&b);
+    multiply=a*b;
+    printf(" sum=%d",multiply);
+}
