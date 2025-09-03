f7698e3f2a28035855a93b8376a80bb02276f2aa
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-09-03 10:25:09 -0700

    Add files via upload

---
+++ print_full_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+ {
+    char a[10], b[10], c[20];
+    printf("Enter your first name:");
+    scanf("%s",a);
+    printf("Enter your middle name:");
+    scanf("%s",b);
+    printf("Enter your last name:");
+    scanf("%s",c);
+    printf(" Your full Name is : %s %s %s",a,b,c);
+
+}
