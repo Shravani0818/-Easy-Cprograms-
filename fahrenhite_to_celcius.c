9c0d772359cfa27ac303616196ecf72cfc498936
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-09-09 21:38:01 +0530

    Add files via upload

---
+++ fahrenhite_to_celcius.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float fahrenhite,celsius;
+    printf("temperatue in farenhite is : ",fahrenhite);
+    scanf("%f",&fahrenhite);
+    celsius=(fahrenhite-32)*5/9;
+    printf("temperatur in celcius is : ",celsius);
+   printf("%f",celsius);
+}
