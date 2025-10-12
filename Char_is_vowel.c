e16ab182bc411dea7f9795fe9ca6000689c11246
Shravani0818 <shravaniwaghmare2006@gmail.com>
2025-10-12 05:19:33 -0700

    Add files via upload

---
+++ Char_is_vowel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+char ch = 'e';
+if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+    printf("Vowel\n");
+else
+    printf("Not a Vowel\n");
+return 0 ;
+}
